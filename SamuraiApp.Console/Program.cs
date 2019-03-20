0f52fe834d1f42aa9f7a658bc8eba303f7cd177d
David Stewart <david.stewart@civica.co.uk>
2019-03-20 08:35:51 +0000

    init

---
+++ SamuraiApp.Console/Program.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SamuraiApp.Data;
+using SamuraiApp.Domain;
+using System;
+using System.Linq;
+
+namespace SamuraiApp.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //InsertSamurai();
+            //InsertMultipleSamurai();
+            SimpleSamuraiQuery();
+        }
+
+        private static void SimpleSamuraiQuery()
+        {
+            using (var context = new SamuraiContext())
+            {
+                var samurais = context.Samurais.ToList();
+
+                // or do something like this
+
+                var query = context.Samurais;
+                var listOfSamurai = query.ToList();
+
+
+            }
+        }
+
+        private static void InsertMultipleSamurai()
+        {
+            var karen = new Samurai { Name = "KDot" };
+            var fletch = new Samurai { Name = "Fleghnog" };
+            var reuben = new Samurai { Name = "Reubs" };
+
+            using (var context = new SamuraiContext())
+            {
+                context.Samurais.AddRange(karen, fletch, reuben);
+                context.SaveChanges();
+            }
+        }
+
+        private static void InsertSamurai()
+        {
+            var samurai = new Samurai { Name = "DS" };
+            using (var context = new SamuraiContext())
+            {
+                context.Samurais.Add(samurai);
+                context.SaveChanges();
+            }
+        }
+    }
+}
