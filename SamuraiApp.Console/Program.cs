95c1b0f8fb4724c2ad09bb6626c1565ed2ac52ed
David Stewart <david.stewart@civica.co.uk>
2019-03-20 09:40:01 +0000

    updates to program

--- SamuraiApp.Console/Program.cs
+++ SamuraiApp.Console/Program.cs
@@ -2,32 +2,59 @@
 using SamuraiApp.Data;
 using SamuraiApp.Domain;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace SamuraiApp.Console
 {
     class Program
     {
+
+        private static SamuraiContext _context = new SamuraiContext();
+
         static void Main(string[] args)
         {
             //InsertSamurai();
             //InsertMultipleSamurai();
-            SimpleSamuraiQuery();
+            //SimpleSamuraiQuery();
+            //DSQuery();
+            AddSamuraiAndQuotes();
+        }
+
+        private static void AddSamuraiAndQuotes()
+        {
+            var samurai = new Samurai
+            {
+                Name = "David", Quotes = new List<Quote> {
+                    new Quote { Text = "Go on"},
+                    new Quote { Text = "Watch out!"}
+                }
+            };
+
+            _context.Add(samurai);
+            _context.SaveChanges();
+        }
+
+        private static void DSQuery()
+        {
+            var name = "DS";
+            var DS = _context.Samurais.Where(s => s.Name == name).ToList();
         }
 
         private static void SimpleSamuraiQuery()
         {
-            using (var context = new SamuraiContext())
-            {
-                var samurais = context.Samurais.ToList();
+            var samurais = _context.Samurais.ToList();
 
-                // or do something like this
+            // or do something like this
 
-                var query = context.Samurais;
-                var listOfSamurai = query.ToList();
+            var query = _context.Samurais;
+            var listOfSamurai = query.ToList();
 
+            var sams = query.Where(s => s.Name == "Kdot");
+            var karen = sams.ToList();
 
-            }
+            //same as all this : 
+            // _context.Samurais.Where(s => s.Name == "Kdot").ToList();
         }
 
         private static void InsertMultipleSamurai()
@@ -36,21 +63,16 @@ namespace SamuraiApp.Console
             var fletch = new Samurai { Name = "Fleghnog" };
             var reuben = new Samurai { Name = "Reubs" };
 
-            using (var context = new SamuraiContext())
-            {
-                context.Samurais.AddRange(karen, fletch, reuben);
-                context.SaveChanges();
-            }
+
+            _context.Samurais.AddRange(karen, fletch, reuben);
+            _context.SaveChanges();
         }
 
         private static void InsertSamurai()
         {
             var samurai = new Samurai { Name = "DS" };
-            using (var context = new SamuraiContext())
-            {
-                context.Samurais.Add(samurai);
-                context.SaveChanges();
-            }
+            _context.Samurais.Add(samurai);
+            _context.SaveChanges();
         }
     }
 }
